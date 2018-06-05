0399ddf5b7111d113171696f181e647a411b7209
Friedemann Neuhaus <friedemann@neuhaus-tech.de>
2018-06-05 06:30:02 +0200

    i3: Change h-split and add local autostart.

--- bin/bin/autostart.sh
+++ bin/bin/autostart.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 feh --bg-fill ~/.wallpaper.jpg
-i3-msg "workspace 1; append_layout ~/.config/i3/workspace-1.json"
-thunderbird &!
 dropbox start
 nm-applet &!
 blueman-applet &!
 [[ -f "/opt/urserver/urserver-start" ]] && /opt/urserver/urserver-start --no-manager
+[[ -f "$HOME/bin/autostart_local.sh" ]] && $HOME/bin/autostart_local.sh
