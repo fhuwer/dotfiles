1c73414109ac55f698905ea54a33acfc7caa6c39
Friedemann Neuhaus <friedemann@neuhaus-tech.de>
2020-02-29 09:30:57 +0100

    notes: Update filters + design.

--- notes/.config/notes/filter.py
+++ notes/.config/notes/filter.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
-from panflute import run_filter, ListContainer, Div
+from panflute import run_filter, ListContainer, Div, Link, Str, Space
+from datetime import datetime
+import os.path
+
+
+def prepare(doc):
+    if os.path.isfile('index.md'):
+        doc.metadata['title'].content.insert(0, Link(Str('↩'), url='index.html'))
+        doc.metadata['title'].content.insert(1, Space())
+    if 'date' in doc.metadata:
+        doc.metadata['date'].content[0].text = datetime.now().strftime('%d.%m.%Y')
 
 
 def finalize(doc):
@@ -11,7 +21,7 @@ def finalize(doc):
     doc.content = list_container
 
 def main(doc=None):
-    return run_filter(lambda x, y: None, doc=doc, finalize=finalize)
+    return run_filter(lambda x, y: None, doc=doc, prepare=prepare, finalize=finalize)
 
 
 if __name__ == "__main__":
