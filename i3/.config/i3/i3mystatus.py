00a1ef36d07d193dc71286498d51d577bb96e545
Friedemann Neuhaus <friedemann@neuhaus-tech.de>
2021-12-28 07:42:22 +0100

    Update i3 config

--- i3/.config/i3/i3mystatus.py
+++ i3/.config/i3/i3mystatus.py
@@ -8,59 +8,60 @@ lanInterface = "eno1"
 
 hostname = os.uname()[1]
 if hostname.startswith("ewpc7"):
-   mobile = False
-   lanInterface = "eno1"
+    mobile = False
+    lanInterface = "eno1"
 if hostname == "sagarmatha":
-   mobile = False
-   lanInterface = "enp3s0"
+    mobile = False
+    lanInterface = "enp3s0"
 
 status = Status()
 
 status.register("clock", format="%a %-d %b %X")
 
-status.register("shell",
-   command = "$HOME/bin/xkblayout-state print %s",
-   interval = 5,
-   on_leftclick = "xkblayout-state set +1",
-   on_rightclick = "run",
+status.register(
+    "shell",
+    command="$HOME/bin/xkblayout-state print %s",
+    interval=5,
+    on_leftclick="xkblayout-state set +1",
+    on_rightclick="run",
 )
 
 if mobile:
-   status.register("battery",
-      format="{status} {percentage:.2f}% - {remaining:%E%h:%M}",
-      alert=True,
-      alert_percentage=5,
-      color="#ffff00",
-      full_color="#009900",
-      critical_color="#ff0000",
-      status={
-         "DIS":  "BAT",
-         "CHR":  "CHR",
-         "FULL": "FUL",
-      }
-   )
+    status.register(
+        "battery",
+        format="{status} {percentage:.2f}% - {remaining:%E%h:%M}",
+        alert=True,
+        alert_percentage=5,
+        color="#ffff00",
+        full_color="#009900",
+        critical_color="#ff0000",
+        status={
+            "DIS": "BAT",
+            "CHR": "CHR",
+            "FULL": "FUL",
+        },
+    )
 
-status.register("load",
-   format="CPU: {avg1} {avg5} {avg15}",
-   color='#839395'
+status.register("load", format="CPU: {avg1} {avg5} {avg15}", color="#839395")
+
+status.register(
+    "mem",
+    format="RAM: {percent_used_mem}%/{total_mem}GiB",
+    divisor=1e9,
+    color="#839395",
+    warn_color="#ca4a15",
+    alert_color="#dc312e",
 )
+#  status.register("taskwarrior")
 
-status.register("mem",
-   format="RAM: {percent_used_mem}%/{total_mem}GiB",
-   divisor=1e9,
-   color='#839395',
-   warn_color='#ca4a15',
-   alert_color='#dc312e'
-)
-status.register("taskwarrior")
-
-status.register("network",
-   interface=lanInterface,
-   format_up="{interface}: {v4cidr} {network_graph}",
-   format_down="{interface}: DOWN",
-   graph_width=20,
-   start_color='#859900',
-   end_color='#dc312e'
+status.register(
+    "network",
+    interface=lanInterface,
+    format_up="{interface}: {v4cidr} {network_graph_recv}",
+    format_down="{interface}: DOWN",
+    graph_width=20,
+    start_color="#859900",
+    end_color="#dc312e",
 )
 
 status.register("pulseaudio", format="♪ {volume}%")
