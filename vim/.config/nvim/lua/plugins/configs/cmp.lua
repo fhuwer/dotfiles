acd0ed6dcac5428e5772503baf43350e53463bb0
Friedemann Neuhaus <friedemann@neuhaus-tech.de>
2022-01-27 07:42:24 +0100

    Update cmp config

--- vim/.config/nvim/lua/plugins/configs/cmp.lua
+++ vim/.config/nvim/lua/plugins/configs/cmp.lua
@@ -6,9 +6,9 @@ end
 
 vim.opt.completeopt = "menuone,noselect"
 
-local function check_back_space()
-  local col = vim.fn.col('.') - 1
-  return col <= 0 or vim.fn.getline('.'):sub(col, col):match('%s')
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 -- nvim-cmp setup
@@ -24,17 +24,6 @@ cmp.setup {
   },
   formatting = {
     format = function(entry, vim_item)
-      -- load lspkind icons
-      -- vim_item.kind = string.format(
-      --   "%s %s",
-      --   require("plugins.configs.lspkind_icons").icons[vim_item.kind],
-      --   vim_item.kind
-      -- )
-      vim_item.kind = string.format(
-        "%s",
-        vim_item.kind
-      )
-
       vim_item.menu = ({
         nvim_lsp = "[LSP]",
         nvim_lua = "[Lua]",
@@ -49,32 +38,38 @@ cmp.setup {
     ["<C-n>"] = cmp.mapping.select_next_item(),
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<PageUp>"] = function(fallback)
+      for i = 1, 10 do
+        cmp.mapping.select_prev_item()(nil)
+      end
+    end,
+    ["<PageDown>"] = function(fallback)
+      for i = 1, 10 do
+        cmp.mapping.select_next_item()(nil)
+      end
+    end,
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.close(),
     ["<CR>"] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
-    ["<Tab>"] = function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-n>", true, true, true), "n")
-      elseif not check_back_space() then
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif has_words_before() then
         cmp.complete()
-      -- elseif require("luasnip").expand_or_jumpable() then
-      --   vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
       else
         fallback()
       end
-    end,
-    ["<S-Tab>"] = function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-p>", true, true, true), "n")
-      -- elseif require("luasnip").jumpable(-1) then
-      --   vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+    end, {"i", "s"}),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
       else
         fallback()
       end
-    end,
+    end, {"i", "s"}),
   },
   sources = {
     { name = "nvim_lsp" },
