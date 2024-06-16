47be45d83d58ea4590a2e71bd50121599d2b3c32
Friedemann Huwer <friedemann@huwer.xyz>
2024-06-16 07:33:03 +0200

    Update indent-blankline config

--- vim/.config/nvim/lua/plugins/configs/indent-blankline.lua
+++ vim/.config/nvim/lua/plugins/configs/indent-blankline.lua
@@ -1,14 +1,17 @@
-require("indent_blankline").setup {
-  indent_blankline_char = "|",
-  space_char_blankline = " ",
-  use_treesitter = true,
-  show_first_indent_level = false,
-  show_trailing_blankline_indent = false,
-  show_current_context = true,
-  show_current_context_start = true,
-  max_indent_increase = 2,
+require("ibl").setup {
+  indent = {
+    char = "|",
+  },
+  -- indent_blankline_char = "|",
+  -- space_char_blankline = " ",
+  -- use_treesitter = true,
+  -- show_first_indent_level = false,
+  -- show_trailing_blankline_indent = false,
+  -- show_current_context = true,
+  -- show_current_context_start = true,
+  -- max_indent_increase = 2,
 }
 
 -- WORKAROUND: until fix of https://github.com/neovim/neovim/issues/14209
 -- see also: https://github.com/lukas-reineke/indent-blankline.nvim/issues/59
-vim.wo.colorcolumn = "99999"
+-- vim.wo.colorcolumn = "99999"
